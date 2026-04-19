4cdf0d9f5a7c21bb38db64da9c1ad2a971c9d0fe
Moritz Schmitz von Hülst <moritz@schmitzvonhuelst.de>
2026-04-19 17:02:44 +0200

    Fix issue in terraform

--- challenge-extra-2/README.md
+++ challenge-extra-2/README.md
@@ -1,5 +1,7 @@
 # Extra Challenge 2: Terraform
 
+I split the manifests to make sure namespaces get created first. Because when I tested this with a local cluster, some manifests couldn't get applied due to the missing namespace.
+
 1. Bootstrap terraform
 ```shell
 terraform init
--- challenge-extra-2/main.tf
+++ challenge-extra-2/main.tf
@@ -3,9 +3,17 @@ locals {
   manifest_paths = fileset(local.prefix, "**/*.yml")
   manifests_raw  = [for data in flatten([for path in local.manifest_paths : split("---", file("${local.prefix}/${path}"))]) : data]
   manifests_decoded = {for k, v in local.manifests_raw: "${lower(yamldecode(v).kind)}/${yamldecode(v).metadata.name}" => yamldecode(v)}
+  namespaces = {for k, v in local.manifests_decoded: k => v if substr(k, 0, length("namespace")) == "namespace" }
+  other_manifests = {for k, v in local.manifests_decoded: k => v if substr(k, 0, length("namespace")) != "namespace" }
 }
 
-resource "kubernetes_manifest" "manifests" {
-  for_each = local.manifests_decoded
+resource "kubernetes_manifest" "namespaces" {
+  for_each = local.namespaces
   manifest = each.value
+}
+
+resource "kubernetes_manifest" "other_manifests" {
+  for_each = local.other_manifests
+  manifest = each.value
+  depends_on = [kubernetes_manifest.namespaces]
 }
